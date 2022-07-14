5c7791dad78c6908d34d507019eec6447fab85e2
lihaohao <2495041749@qq.com>
2022-07-14 23:01:22 +0800

    feat: 增加底部布局

---
+++ src/components/DragFooter/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="drag-footer">
+
+  </div>
+</template>
+
+<style lang="less" scoped></style>
--- src/views/Drag/index.vue
+++ src/views/Drag/index.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import Left from './components/Left'
 import Right from './components/Right'
+import DragFooter from '@/components/DragFooter/index.vue'
 import defaultBlocks from './editor-data'
 import { IBlockItem } from '@/types'
 
@@ -11,16 +12,32 @@ const allBlockItem = ref<IBlockItem[]>(defaultBlocks)
 
 <template>
   <div class="drag full-height">
-    <Left v-model="allBlockItem" />
-    <Right v-model="allBlockItem" />
+    <div class="drag-top">
+      <Left v-model="allBlockItem" />
+      <Right v-model="allBlockItem" />
+    </div>
+    <div class="drag-bottom">
+      <DragFooter />
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
 .drag {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
   width: 100%;
+
+  &-top {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    width: 100%;
+    height: calc(100% - 50px);
+  }
+
+  &-bottom {
+    background: black;
+    height: 40px;
+    margin-top: 10px;
+  }
 }
 </style>
